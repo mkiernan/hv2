7737e9f7bc60f64498272a6f25885f78c275d6fc
Mike Kiernan <mkiernan@microsoft.com>
2019-02-04 21:38:51 +0000

    fix extension

--- hb60rs_extension.sh
+++ hb60rs_extension.sh
@@ -26,37 +26,23 @@ wget https://aka.ms/lis
 tar xzf lis
 pushd LISISO
 ./upgrade.sh
-yum update -y WALinuxAgent
 popd
 rm -rf lis; rm -rf LISISO
 
 #install mellanox driver
+yum install -y numactl
 yum install -y kernel-devel python-devel
-yum install -y kernel-devel-3.10.0-957.1.3.el7.x86_64
+#yum install -y kernel-devel-3.10.0-957.1.3.el7.x86_64
 yum install -y redhat-rpm-config rpm-build gcc-gfortran gcc-c++
 yum install -y gtk2 atk cairo tcl tk createrepo
 wget http://content.mellanox.com/ofed/MLNX_OFED-4.5-1.0.1.0/MLNX_OFED_LINUX-4.5-1.0.1.0-rhel7.6-x86_64.tgz
 tar zxvf MLNX_OFED_LINUX-4.5-1.0.1.0-rhel7.6-x86_64.tgz
 ./MLNX_OFED_LINUX-4.5-1.0.1.0-rhel7.6-x86_64/mlnxofedinstall --add-kernel-support
+sed -i 's/LOAD_EIPOIB=no/LOAD_EIPOIB=yes/g' /etc/infiniband/openib.conf
 /etc/init.d/openibd restart
 rm -rf ./MLNX_*
 
-# Use WALinuxAgent to assign IP address and make it persist
-yum install -y python-setuptools
-yum install -y git
-git clone https://github.com/Azure/WALinuxAgent.git
-pushd WALinuxAgent
-wget https://patch-diff.githubusercontent.com/raw/Azure/WALinuxAgent/pull/1365.patch
-wget https://patch-diff.githubusercontent.com/raw/Azure/WALinuxAgent/pull/1375.patch
-wget https://patch-diff.githubusercontent.com/raw/Azure/WALinuxAgent/pull/1389.patch
-git reset --hard 72b643ea93e5258c3cec0e778017936806111f15
-git am 1*.patch
-python setup.py install --register-service
-sed -i -e 's/# OS.EnableRDMA=y/OS.EnableRDMA=y/g' /etc/waagent.conf
-sed -i -e 's/AutoUpdate.Enabled=y/# AutoUpdate.Enabled=y/g' /etc/waagent.conf
-systemctl restart waagent
-popd
-rm -rf WALinuxAgent
+yum update -y WALinuxAgent
 
 # install intel mpi 2018.4 - find this a bit too slow for an extension also, so bake it into the image.
 # https://software.intel.com/en-us/articles/installing-intel-free-libs-and-python-yum-repo
@@ -68,8 +54,6 @@ yum -y update
 #yum -y install intel-mkl-2018.4-274
 yum -y install intel-mkl-2018.4-057
 yum -y install intel-mpi-2018.4-057
-
-
 
 #automatically reclaim memory to avoid remote memory access 
 echo 1 >/proc/sys/vm/zone_reclaim_mode
